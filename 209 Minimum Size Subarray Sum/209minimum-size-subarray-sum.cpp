afeba1f47207cbcf0bc59e2dde03ddf941648f64
mayankraj0508 <mrajdbg2018@gmail.com>
2024-08-02 11:49:25 +0000

    Code challenge submission for question: 209 Minimum Size Subarray Sum

---
+++ 209 Minimum Size Subarray Sum/209minimum-size-subarray-sum.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int n = nums.size();
+        int length =0;
+        int sum  =0;
+        int minlength = INT_MAX;
+        int i =0;
+        int j =0;
+        while(j<n){
+            sum  = sum+ nums[j];
+
+        
+        while(sum>=target){
+            length = j-i+1;
+            minlength = min(minlength,length);
+            sum = sum-nums[i];
+            i++;
+        }
+        j++;
+        }
+        if(minlength==INT_MAX){
+            return 0;
+        }
+        else{
+        return minlength;
+        }
+
+
+        
+    }
+};
