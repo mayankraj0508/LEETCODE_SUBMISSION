2b2ce1fbc2d182d07d6cf515dd50d83907d7c2d7
mayankraj0508 <mrajdbg2018@gmail.com>
2025-10-31 14:09:54 +0000

    Code challenge submission for question: 88 Merge Sorted Array

---
+++ 88 Merge Sorted Array/88merge-sorted-array.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void merge(vector<int>& arr1, int m, vector<int>& arr2, int n) {
+    int i = m-1;
+    int j =n-1;
+    int k= m+n-1;
+    while(i>=0&&j>=0){
+        if(arr1[i]>arr2[j]){
+            arr1[k] = arr1[i];
+            i--;
+            k--;
+        }
+        else{
+            arr1[k] = arr2[j];
+            j--;
+            k--;
+        }
+    }
+    while(i>=0){
+        arr1[k] = arr1[i];
+        i--;
+        k--;
+    }
+    while(j>=0){
+        arr1[k] = arr2[j];
+        j--;
+        k--;
+    }
+
+        
+    }
+};
