83ae9e22fa3c7b9e18495321d7617b83bbf7bf17
mayankraj0508 <mrajdbg2018@gmail.com>
2025-12-20 04:35:05 +0000

    Code challenge submission for question: 965 Univalued Binary Tree

---
+++ 965 Univalued Binary Tree/965univalued-binary-tree.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool check(TreeNode*root,int &v){
+        if(root==NULL){
+            return true;
+        }
+        if(root->val!=v){
+            return false;
+        }
+        return check(root->left,v)&&check(root->right,v);
+    }
+    bool isUnivalTree(TreeNode* root) {
+        int va = root->val;
+        return check(root,va);
+        
+    }
+};
