6e79b919276c1a3ba3c86a8db253a6a0ecdcd5f1
mayankraj0508 <mrajdbg2018@gmail.com>
2026-04-23 05:09:31 +0000

    Code challenge submission for question: 1929 Concatenation of Array

---
+++ 1929 Concatenation of Array/1929concatenation-of-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<int>v(2*n);
+        for(int i = 0; i<v.size(); i++){
+            if(i<=n-1){
+                v[i] = nums[i];
+            }
+            else{
+                v[i] = nums[i-n];
+            }
+        }
+        return v;
+
+        
+    }
+};
