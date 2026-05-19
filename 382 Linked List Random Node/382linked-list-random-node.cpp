56fac47a0dfa86707b51b46593beef3e4e21d74e
mayankraj0508 <mrajdbg2018@gmail.com>
2026-05-19 06:33:37 +0000

    Code challenge submission for question: 382 Linked List Random Node

---
+++ 382 Linked List Random Node/382linked-list-random-node.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    
+    vector<int>v;
+
+    Solution(ListNode* head) {
+        ListNode*temp = head;
+        while(temp){
+            v.push_back(temp->val);
+            temp = temp->next;
+        }
+    }
+    
+    int getRandom() {
+        int n = v.size();
+        int idx = rand()%n;
+        return v[idx];
+    }
+};
