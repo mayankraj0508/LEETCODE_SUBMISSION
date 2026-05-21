402aeb29c1616ed57d07a842af63c62098bfcc52
mayankraj0508 <mrajdbg2018@gmail.com>
2026-05-21 14:32:48 +0000

    Code challenge submission for question: 697 Degree of an Array

---
+++ 697 Degree of an Array/697degree-of-an-array.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int findShortestSubArray(vector<int>& nums) {
+
+        unordered_map<int,int> freq;
+        unordered_map<int,int> first;
+        unordered_map<int,int> last;
+
+        int degree = 0;
+
+        for(int i=0; i<nums.size(); i++){
+
+            if(first.find(nums[i]) == first.end()){
+                first[nums[i]] = i;
+            }
+
+            last[nums[i]] = i;
+
+            freq[nums[i]]++;
+
+            degree = max(degree, freq[nums[i]]);
+        }
+
+        int ans = INT_MAX;
+
+        for(auto x : freq){
+
+            if(x.second == degree){
+
+                ans = min(ans,
+                          last[x.first] - first[x.first] + 1);
+            }
+        }
+
+        return ans;
+    }
+};
