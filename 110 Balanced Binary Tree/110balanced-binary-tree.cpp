1e6f92db9867201ae4a91c31197087a334fe1c88
mayankraj0508 <mrajdbg2018@gmail.com>
2026-06-27 11:57:45 +0000

    Code challenge submission for question: 110 Balanced Binary Tree

---
+++ 110 Balanced Binary Tree/110balanced-binary-tree.cpp
@@ -0,0 +1,65 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+int find(TreeNode*root){
+    if(root==NULL){
+        return 0;
+    }
+    int lh = find(root->left);
+   
+    int rh = find(root->right);
+
+    if(abs(lh-rh)>1){
+        cout<<lh;
+        return -1;
+    }
+    return max(lh,rh)+1;
+}
+// bool f(TreeNode*root){
+//     if(root==NULL){
+//         return true;
+//     }
+//     int  ans = abs(find(root->left)-find(root->right));
+//     if(ans>1){
+//        return false;
+//     }
+//     return f(root->left)&&f(root->right);
+// }
+
+   int f(TreeNode*root){
+     if(!root){
+        return 0;
+     }
+     int left = f(root->left);
+     if(left==-1){
+        return -1;
+     }
+     int right  = f(root->right);
+     if(right==-1){
+        return -1;
+     }
+     if(abs(left-right)>1){
+        return -1;
+     }
+     return 1+max(left,right);
+
+   }
+    bool isBalanced(TreeNode* root) {
+        int trp =  f(root);
+         if(trp<=-1){
+            return false;
+         }
+         return true;
+        
+    }
+};
